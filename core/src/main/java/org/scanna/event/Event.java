ee924388681231266eef4f9a84084ea6ac3a5613
simonpai <simonpai@potix.com>
2012-08-22 17:53:08 +0800

    Enhance Event.

--- core/src/main/java/org/scanna/event/Event.java
+++ core/src/main/java/org/scanna/event/Event.java
@@ -9,22 +9,33 @@ import org.scanna.Line;
  * TODO
  * @author simonpai
  */
-public class Event {
+public class Event implements Comparable<Event> {
 	
 	protected final Line _line;
-	protected final int _col;
+	protected final int _col, _row;
 	
 	public Event(Line line, int col) {
 		_line = line;
 		_col = col;
+		_row = line.getRow();
 	}
 	
 	public Line getLine() {
 		return _line;
 	}
 	
+	public int getRow() {
+		return _row;
+	}
+	
 	public int getColumn() {
 		return _col;
+	}
+	
+	@Override
+	public int compareTo(Event evt) {
+		int rdiff = _row - evt._row;
+		return rdiff != 0 ? rdiff : _col - evt._col;
 	}
 	
 }
